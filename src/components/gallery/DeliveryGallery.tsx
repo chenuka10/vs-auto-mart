c41cb3e1fb9954c35def9a100ac93a52d294b2f3
chenuka10 <chenukaranthila@gmail.com>
2026-09-08 09:24:15 +0530

    deployment bug fix

--- src/components/gallery/DeliveryGallery.tsx
+++ src/components/gallery/DeliveryGallery.tsx
@@ -8,16 +8,8 @@ interface DeliveryGalleryProps {
 
 export function DeliveryGallery({ items }: DeliveryGalleryProps) {
   return (
-    <div style={{ height: "650px", position: "relative" }}>
-      <CircularGallery
-        items={items}
-        bend={2}
-        textColor="#EDEDE5"
-        borderRadius={0.06}
-        fontUrl="https://fonts.googleapis.com/css2?family=Oswald:wght@600&display=swap"
-        font="600 22px Oswald"
-        scrollEase={0.04}
-      />
+    <div className="relative h-[650px] w-full">
+      <CircularGallery items={items} />
     </div>
   );
 }
